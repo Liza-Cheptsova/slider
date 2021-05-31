e34820c1447d0b947a928db5efe4a6269a0e4945
Liza-Cheptsova <you@example.com>
2021-05-31 16:41:46 +0300

    add slider

---
+++ src/slider/Slider.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import s from "../App.module.css";
+import { SliderData } from "./../sliderData/SliderData";
+
+export const Slider = ({ slides }: any) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const length = slides.length;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((current: any) => {
+        const res = current === length - 1 ? 0 : activeIndex + 1;
+        return res;
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [activeIndex, length]);
+
+  const nextSlideHandlet = () => {
+    setActiveIndex(activeIndex === length - 1 ? 0 : activeIndex + 1);
+  };
+  const prevSlideHandlet = () => {
+    setActiveIndex(activeIndex === 0 ? length - 1 : activeIndex - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) return null;
+
+  return (
+    <div className={s.slider}>
+      <button className={s.prev_btn} onClick={prevSlideHandlet}>
+        &#11144;
+      </button>
+      {SliderData.map((slide, index) => (
+        <div className={index === activeIndex ? s.slide + " " + s.active : s.slide} key={index}>
+          {index === activeIndex && <img src={slide.image} className={s.actve_img} alt='woods' />}
+        </div>
+      ))}
+      <button className={s.next_btn} onClick={nextSlideHandlet}>
+        &#11146;
+      </button>
+    </div>
+  );
+};
---
+++ src/sliderData/SliderData.ts
@@ -0,0 +1,46 @@
+export const SliderData = [
+  {
+    image:
+      "https://images.pexels.com/photos/302804/pexels-photo-302804.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/1367192/pexels-photo-1367192.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/192136/pexels-photo-192136.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/880675/pexels-photo-880675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/167684/pexels-photo-167684.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/1292115/pexels-photo-1292115.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/235621/pexels-photo-235621.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/247421/pexels-photo-247421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/589840/pexels-photo-589840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+];
+
+// export type SliderDataType = {
+//   image: string;
+// };
